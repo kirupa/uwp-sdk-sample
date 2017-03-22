f2aa0cb284fafcea9a6c8e85684096d81e184c6d
John-David Dalton <john.david.dalton@gmail.com>
2017-03-21 18:01:32 -0700

    Add ability to cycle through dropdown entries.

--- BackgroundMediaPlayback/js/js/main.js
+++ BackgroundMediaPlayback/js/js/main.js
@@ -13,18 +13,44 @@ document.addEventListener('DOMContentLoaded', () => {
     const mediaPlayer = new MediaPlayer
     mediaPlayer.autoPlay = false
 
-    const frag = document.createDocumentFragment()
-    const scratchpad = frag.appendChild(document.createElement('div'))
+    const bar = document.querySelector('#control-panel')
+    const drop = document.querySelector('#dropdownMenu1').parentNode
+    const dropLabel = drop.querySelector('.dropdown-toggle')
+    const dropMenu = document.querySelector('#dropdownMenu1 + .dropdown-menu')
+    const dropItems = dropMenu.getElementsByTagName('li')
+
+    Object.defineProperty(drop, 'selectedIndex', (() => {
+       let selectedIndex = -1
+       const overflow = (n, min, max) => {
+            const range = max - min + 1
+            let mod = (n - min) % range
+            if (mod < 0) {
+                mod += range
+            }
+            return min + mod
+        }
+        return {
+            configurable: true,
+            enumerable: true,
+            get() {
+                return selectedIndex
+            },
+            set(index) {
+                const lastIndex = Math.max(0, dropItems.length - 1)
+                selectedIndex = overflow(index, 0, lastIndex)
+                const { innerText } = dropItems[selectedIndex]
+                dropLabel.innerHTML = `${innerText} <span class="caret"></span>`
+            }
+        }
+    })())
 
     Storage.StorageFile.getFileFromApplicationUriAsync(new Uri('ms-appx:///assets/playlist.json'))
         .then((storageFile) => Storage.FileIO.readTextAsync(storageFile))
         .then((jsonText) => {
-            const bar = document.querySelector('#control-panel')
-            const menu = document.querySelector('#dropdownMenu1 + .dropdown-menu')
-            const items = JSON.parse(jsonText).mediaList.items
             const playlist = new MediaPlaybackList
             playlist.autoRepeatEnabled = true
 
+            const items = JSON.parse(jsonText).mediaList.items
             items.forEach((object, index) => {
                 const source = MediaSource.createFromUri(new Uri(object.mediaUri))
                 const item = new MediaPlaybackItem(source)
@@ -34,16 +60,28 @@ document.addEventListener('DOMContentLoaded', () => {
                 item.applyDisplayProperties(props)
                 playlist.items.append(item)
 
-                scratchpad.innerHTML = `<li><a href="#">${object.title}</a></li>`
-                menu.appendChild(scratchpad.firstChild)
-                  .addEventListener('click', () => mediaPlayer.source.moveTo(index))
+                const li = document.createElement('li')
+                li.innerHTML = `<a href="#">${object.title}</a>`
+                li.addEventListener('click', () => mediaPlayer.source.moveTo(index))
+                dropMenu.appendChild(li)
             })
 
             mediaPlayer.source = playlist
-            bar.querySelector('.glyphicon-play').addEventListener('click',  () => mediaPlayer.play())
-            bar.querySelector('.glyphicon-pause').addEventListener('click', () => mediaPlayer.pause())
-            bar.querySelector('.glyphicon-forward').addEventListener('click',  () => mediaPlayer.source.moveNext())
-            bar.querySelector('.glyphicon-backward').addEventListener('click',  () => mediaPlayer.source.movePrevious())
+
+            bar.querySelector('.glyphicon-play').parentNode.addEventListener('click', () => {
+                mediaPlayer.play()
+            })
+            bar.querySelector('.glyphicon-pause').parentNode.addEventListener('click', () => {
+                mediaPlayer.pause()
+            })
+            bar.querySelector('.glyphicon-forward').parentNode.addEventListener('click', () => {
+                drop.selectedIndex++
+                mediaPlayer.source.moveNext()
+            })
+            bar.querySelector('.glyphicon-backward').parentNode.addEventListener('click', () => {
+                drop.selectedIndex--
+                mediaPlayer.source.movePrevious()
+            })
         })
 
     document.querySelector('#menu-toggle').addEventListener('click', (e) => {
@@ -52,10 +90,11 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     document.querySelector('.dropdown-menu').addEventListener('click', ({ target }) => {
-        if (target.matches('li a')) {
-            const text = target.textContent
-            const dropdown = target.parentNode.parentNode.parentNode
-            dropdown.querySelector('.dropdown-toggle').innerHTML = `${text} <span class="caret"></span>`
+        if (target.msMatchesSelector('li a')) {
+            let index = 0
+            let node = target.parentNode
+            while ((node = node.previousElementSibling)) index++
+            drop.selectedIndex = index
         }
     })
 })
